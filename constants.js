6da1182bea132e9c55d53ac7167292649a3e5b06
Andrej Maslovara <amaslovara@gmail.com>
2024-10-05 08:51:54 +0200

    basically a copy of the full version with little add-ons

--- constants.js
+++ constants.js
@@ -1,41 +1,94 @@
-const MIN_NUMBER = 1;
-const MAX_NUMBER = 100;
-const CLOSE_RANGE = 10;
-const MAX_ATTEMPTS = 10;
+//Local Storage
+export const LOCAL_STORAGE_KEY = 'state';
 
-const GUESS_PROMPT_MESSAGE = `Enter your guess (a number between ${MIN_NUMBER} and ${MAX_NUMBER}).`;
-const INVALID_NUMBER_MESSAGE = `Invalid input. Please enter a number between ${MIN_NUMBER} and ${MAX_NUMBER}.`;
-const MAX_ATTEMPTS_REACHED_MESSAGE = `You've reached the maximum number of ${MAX_ATTEMPTS} attempts. Better luck next time!`;
-const LOW_GUESS_MESSAGE = "Your guess is too low.";
-const LOW_GUESS_CLOSE_MESSAGE = "You're close! Your guess is low.";
-const HIGH_GUESS_MESSAGE = "Your guess is too high.";
-const HIGH_GUESS_CLOSE_MESSAGE = "You're close! Your guess is high.";
-const CORRECT_GUESS_MESSAGE = "Congratulations! You guessed it right!";
-const TRY_AGAIN_MESSAGE = "Would you like to try again?";
-const THANKS_FOR_PLAYING_MESSAGE = "Thanks for playing!";
-const WELCOME_MESSAGE = `
+// Game Settings
+export const GAME_SETTINGS = {
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 100,
+  CLOSE_RANGE: 10,
+  MAX_ATTEMPTS: 10,
+  INITIAL_DELAY: 1000,
+};
+
+// Prompt Messages
+export const PROMPTS_MESSAGES = {
+  GUESS_PROMPT: `Enter your guess (a number between ${GAME_SETTINGS.MIN_NUMBER} and ${GAME_SETTINGS.MAX_NUMBER}).`,
+  TRY_AGAIN: "Would you like to try again?",
+  NEXT_ROUND: "Proceed to next round?",
+};
+
+// Error Messages
+export const ERROR_MESSAGES = {
+  INVALID_NUMBER: `Invalid input. Please enter a number between ${GAME_SETTINGS.MIN_NUMBER} and ${GAME_SETTINGS.MAX_NUMBER}.`,
+  NOT_A_NUMBER: "Only numbers are accepted. Please try again.",
+  NOT_AN_INTEGER: "Please enter an integer value.",
+  OUT_OF_RANGE_LOW: `Please enter a number greater than or equal to ${GAME_SETTINGS.MIN_NUMBER}.`,
+  OUT_OF_RANGE_HIGH: `Please enter a number less than or equal to ${GAME_SETTINGS.MAX_NUMBER}.`,
+  NOT_EMPTY_SPACE: "empty space is not accepted",
+  GAME_INITIALIZATION:
+    "Something went wrong!. Please refresh the page and try again.",
+};
+
+// Feedback Messages
+export const FEEDBACK_MESSAGES = {
+  LOW_GUESS: "Your guess of ${playerGuess} is too low.",
+  LOW_GUESS_CLOSE: "You're close! Your guess of ${playerGuess} is low.",
+  HIGH_GUESS: "Your guess of ${playerGuess} is too high.",
+  HIGH_GUESS_CLOSE: "You're close! Your guess of ${playerGuess} is high.",
+  CORRECT_GUESS: "Congratulations! You guessed it right!",
+  MAX_ATTEMPTS_REACHED: `You've reached the maximum number of ${GAME_SETTINGS.MAX_ATTEMPTS} attempts. Better luck next time! The correct number was: `,
+};
+
+// Game Flow Messages
+export const GAME_FLOW_MESSAGES = {
+  WELCOME: `
     🎉 Welcome to the Ultimate Number Guessing Game! 🎉
     Here are the rules:
-    - You have up to ${MAX_ATTEMPTS} attempts to guess the correct number.
-    - The number will be between ${MIN_NUMBER} and ${MAX_NUMBER}.
+    - You have up to ${GAME_SETTINGS.MAX_ATTEMPTS} attempts to guess the correct number.
+    - The number will be between ${GAME_SETTINGS.MIN_NUMBER} and ${GAME_SETTINGS.MAX_NUMBER}.
     🔍 Pay close attention to the hints after each guess!
     Can you find the correct number? Good luck, and have fun!
-  `;
+  `,
+  THANKS_FOR_PLAYING: "Thanks for playing!",
+  GAME_CANCELLED: "The game has been cancelled",
+  GAME_START: "Game is starting...",
+  GAME_RESTART: "If you want to restart the game, type in 'restart'.",
+  GAME_RESTARTED: "The game has been restarted. Good luck with the new round!",
+};
 
-export {
-  INVALID_NUMBER_MESSAGE,
-  LOW_GUESS_MESSAGE,
-  LOW_GUESS_CLOSE_MESSAGE,
-  HIGH_GUESS_MESSAGE,
-  HIGH_GUESS_CLOSE_MESSAGE,
-  CORRECT_GUESS_MESSAGE,
-  GUESS_PROMPT_MESSAGE,
-  CLOSE_RANGE,
-  MAX_ATTEMPTS,
-  MAX_ATTEMPTS_REACHED_MESSAGE,
-  MIN_NUMBER,
-  MAX_NUMBER,
-  TRY_AGAIN_MESSAGE,
-  THANKS_FOR_PLAYING_MESSAGE,
-  WELCOME_MESSAGE,
+// Score Messages
+export const SCORE_MESSAGES = {
+  INCREDIBLE: "Incredible! You achieved an outstanding result! your score for this round is ",
+  GREAT_JOB:
+    "Great job! Your result reflects your impressive skills! your score for this round is ",
+  GOOD_JOB: "Good work! You've got a solid result! your score for this round is ",
+  TOOK_SEVERAL_ATTEMPTS:
+    "Well done! Your result shows your perseverance! your score for this round is ",
+  BETTER_LUCK_NEXT_TIME: "Don't worry, you'll get it next time! your score for this round is ",
+  GAME_CANCELLED: "Your score for this round is ",
+  TOTAL_SCORE: "Your total score is ",
+};
+
+// Score Ranges
+export const SCORE_DETAILS = [
+  { MAX_ATTEMPTS: 3, SCORE: 60, MESSAGE: SCORE_MESSAGES.INCREDIBLE },
+  { MAX_ATTEMPTS: 5, SCORE: 40, MESSAGE: SCORE_MESSAGES.GREAT_JOB },
+  { MAX_ATTEMPTS: 8, SCORE: 20, MESSAGE: SCORE_MESSAGES.GOOD_JOB },
+  {
+    MAX_ATTEMPTS: 10,
+    SCORE: 10,
+    MESSAGE: SCORE_MESSAGES.TOOK_SEVERAL_ATTEMPTS,
+  },
+];
+
+// Default score and message for attempts greater than 10
+export const GAME_OVER_SCORE_DETAIL = {
+  SCORE: 0,
+  MESSAGE: SCORE_MESSAGES.BETTER_LUCK_NEXT_TIME,
+};
+
+// Score for canceled game
+export const CANCELED_GAME_SCORE_DETAIL = {
+  SCORE: 0,
+  MESSAGE: SCORE_MESSAGES.GAME_CANCELLED,
 };
---
+++ gameState.js
@@ -0,0 +1,11 @@
+export const DEFAULT_STATE = {
+  rounds: [],
+  currentRound: {
+    roundNumber: 1,
+    attempts: 0,
+    hasWon: false,
+    score: 0,
+    correctNumber: null,
+    isCancelled: false,
+  },
+};
